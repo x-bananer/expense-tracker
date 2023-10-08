14a09666fca195a1e052f525a87072449eb7a0f5
Ксения Шлёнская <k.shlenskaya@duotek.ru>
2023-10-08 13:48:57 +0300

    Rendering lists of data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,10 +16,7 @@ const Expenses = ({ expenses }) => {
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onApplyFilter={filterChangeHandler} />
-                <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-                <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-                <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-                <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+                { expenses.map((expense, index) => <ExpenseItem key={index} title={expense.title} amount={expense.amount} date={expense.date} />)}
             </Card>
         </div>
     )
